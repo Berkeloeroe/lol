7e3f93130eb4b406b4257a663a9010e21ff28560
Randy <54804029+Berkeloeroe@users.noreply.github.com>
2025-06-15 14:45:11 +0200

    a

--- scripts/script.js
+++ scripts/script.js
@@ -15,3 +15,26 @@ document.addEventListener('DOMContentLoaded', () => {
 
   
 });
+
+ const openBtn = document.querySelector('.open-menu');
+        const closeBtn = document.querySelector('.close-menu');
+        const nav = document.querySelector('.main-nav');
+        const overlay = document.querySelector('.overlay');
+
+        openBtn.addEventListener('click', () => {
+            nav.classList.add('open');
+            overlay.classList.add('show');
+            nav.setAttribute('aria-hidden', 'false');
+        });
+
+        closeBtn.addEventListener('click', () => {
+            nav.classList.remove('open');
+            overlay.classList.remove('show');
+            nav.setAttribute('aria-hidden', 'true');
+        });
+
+        overlay.addEventListener('click', () => {
+            nav.classList.remove('open');
+            overlay.classList.remove('show');
+            nav.setAttribute('aria-hidden', 'true');
+        });
